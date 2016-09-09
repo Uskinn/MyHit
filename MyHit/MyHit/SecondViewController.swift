a0d197a6e1f3a6bdb74ee19d6bb99b868031ac24
Uskinn <s.nevzor@yahoo.com>
2016-09-09 09:52:31 -0400

    file organization

---
+++ MyHit/MyHit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyHit
+//
+//  Created by Sergey Nevzorov on 9/3/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
