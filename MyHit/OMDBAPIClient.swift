01a545d0b225613c23109a11bbea882aeb272ef7
Uskinn <s.nevzor@yahoo.com>
2016-09-19 20:06:02 -0400

    small changes in the collectionView

--- MyHit/OMDBAPIClient.swift
+++ MyHit/OMDBAPIClient.swift
@@ -10,43 +10,43 @@ import UIKit
 
 class OMDBAPIClient: NSObject {
     
-    class func getMovieWithComplition(complitionHandler: (Movie) -> ()) {
-        
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        
-        if let OMDBURL = NSURL(string: "http://www.omdbapi.com/?t=taxi&y=&plot=full&r=json" ) {
-            
-            let OMDBTask = session.dataTaskWithURL(OMDBURL, completionHandler: { (data, response, error) in
-                
-                if let data = data {
-                    do {
-                        
-                        let responseData = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-                            as! NSDictionary
-                        
-                        NSOperationQueue.mainQueue().addOperationWithBlock({
-                            
-                            let movie = Movie.mapFromDictionary(responseData)
-                            
-                            complitionHandler(movie)
-                            print(responseData)
-                        })
-                        
-                    } catch {
-                        print("Error: \(error)")
-                    }
-                }
-            })
-            
-            OMDBTask.resume()
-        }
-    }
+//    class func getMovieWithComplition(complitionHandler: (Movie) -> ()) {
+//        
+//        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+//        
+//        if let OMDBURL = NSURL(string: "http://www.omdbapi.com/?t=taxi&y=&plot=full&r=json" ) {
+//            
+//            let OMDBTask = session.dataTaskWithURL(OMDBURL, completionHandler: { (data, response, error) in
+//                
+//                if let data = data {
+//                    do {
+//                        
+//                        let responseData = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+//                            as! NSDictionary
+//                        
+//                        NSOperationQueue.mainQueue().addOperationWithBlock({
+//                            
+//                            let movie = Movie.mapFromDictionary(responseData)
+//                            
+//                            complitionHandler(movie)
+//                            print(responseData)
+//                        })
+//                        
+//                    } catch {
+//                        print("Error: \(error)")
+//                    }
+//                }
+//            })
+//            
+//            OMDBTask.resume()
+//        }
+//    }
     
     class func getMovies(completion: ([Movie] -> Void)) {
         
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         
-        if let  omdbURL = NSURL(string: "http://www.omdbapi.com/?s=love&page=2") {
+        if let  omdbURL = NSURL(string: "http://www.omdbapi.com/?s=taxi&page=2") {
             let omdbTask = session.dataTaskWithURL(omdbURL, completionHandler: { (data, response, error ) in
                 if let data = data {
                     do {
