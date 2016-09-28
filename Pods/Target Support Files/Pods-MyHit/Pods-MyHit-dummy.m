4bad88a571265626665c7abacaea9c2f25422a1b
Uskinn <s.nevzor@yahoo.com>
2016-09-28 10:45:58 -0400

    deleted all master files

--- Pods/Target Support Files/Pods-MyHit/Pods-MyHit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyHit : NSObject
-@end
-@implementation PodsDummy_Pods_MyHit
-@end
--- Pods/Target Support Files/Pods-MyHit/Pods-MyHit-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_MyHitVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MyHitVersionString[];
-
