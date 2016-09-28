102b1d8a54b3a560e7817b1722781ccd54fb4f2c
Uskinn <s.nevzor@yahoo.com>
2016-09-28 10:34:06 -0400

    recreated NSManagedObject

--- MyHit/ManagedMovie+CoreDataProperties.swift
+++ MyHit/ManagedMovie+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  ManagedMovie+CoreDataProperties.swift
-//  
+//  MyHit
 //
-//  Created by Sergey Nevzorov on 9/27/16.
-//
+//  Created by Sergey Nevzorov on 9/28/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -19,12 +19,12 @@ extension ManagedMovie {
     @NSManaged var genre: String?
     @NSManaged var imdbID: String?
     @NSManaged var imdbRating: String?
+    @NSManaged var isFavorite: NSNumber?
     @NSManaged var metaScore: String?
     @NSManaged var plot: String?
     @NSManaged var poster: String?
     @NSManaged var title: String?
     @NSManaged var writer: String?
     @NSManaged var year: String?
-    @NSManaged var isFavorite: Bool
 
 }
--- MyHit/ManagedMovie.swift
+++ MyHit/ManagedMovie.swift
@@ -1,9 +1,9 @@
 //
 //  ManagedMovie.swift
-//  
+//  MyHit
 //
-//  Created by Sergey Nevzorov on 9/24/16.
-//
+//  Created by Sergey Nevzorov on 9/28/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
 //
 
 import Foundation
@@ -11,7 +11,6 @@ import CoreData
 
 
 class ManagedMovie: NSManagedObject {
-    
 
 // Insert code here to add functionality to your managed object subclass
 
